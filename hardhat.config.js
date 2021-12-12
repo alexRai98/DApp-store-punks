40090b68e1945ca168085def31d4caec2b3aca76
rai_delgado <alexander.delgado@kushkipagos.com>
2021-12-12 01:36:09 -0500

    project init

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+const projectID = process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+
+module.exports = {
+  solidity: "0.8.4",
+  networks:{
+    rinkedy : {
+      url:`https://rinkeby.infura.io/v3/${projectID}`,
+      accounts: [
+        privateKey
+      ]
+    }
+  }
+};
